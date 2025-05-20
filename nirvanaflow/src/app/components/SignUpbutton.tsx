11cc7a516d423534a7443b0dfe4f4f074bb7631d
Abir panda <abirpanda335@gmail.com>
2025-05-20 09:38:50 +0530

    minor fixes

--- nirvanaflow/src/app/components/SignUpbutton.tsx
+++ nirvanaflow/src/app/components/SignUpbutton.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { FcGoogle } from 'react-icons/fc';
 
-export default function SignUpButton(){
+export default function SignUpbutton(){
     return(
         
       <button className="flex items-center justify-center w-full gap-3 border border-zinc-700 py-3 px-4 rounded-lg bg-zinc-900 hover:bg-zinc-800 transition-all hover:shadow-md">
